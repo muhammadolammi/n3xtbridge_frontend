8777364a94ee211f907e618131993b117bc03e3c
Muhammad Akewukanwo <muhammadolammi@gmail.com>
2026-03-21 14:40:57 +0100

    redirect firbase domains

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+
+if (
+  window.location.hostname.endsWith("web.app") ||
+  window.location.hostname.endsWith("firebaseapp.com")
+) {
+  window.location.replace("https://n3xtbridge.com");
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
